04ce887693d1d2d46456e7833d8c01d21e55a5b9
smailinfo <smailinfo31@gmail.com>
2020-10-07 15:44:05 +0000

    follow thread

--- status_process.py
+++ status_process.py
@@ -3,7 +3,7 @@ import subprocess
 import threading
 import time
 import psycopg2
-
+import multiprocessing
 from prettytable import PrettyTable
 from termcolor import colored
 from datetime import datetime, timedelta ,timezone
@@ -53,58 +53,96 @@ def foo():
 
 
 
-task_list=[]
-q = queue.Queue()
-
-
-
-
-sql = (f'''SELECT task_number , task_name , task_time_start  FROM groundstation.planifier
-       where planifier."task_time_start" > now() at time zone 'utc'
-       order by planifier."task_time_start"
-       asc limit 10
-       ''')
-curr.execute(sql)
-rows = curr.fetchall()
-for row in rows:
-    task_list.append(row)
-    q.put(row)
-
-while True:
-    print ("#####################################################################")
-    for head in list(q.queue):
-        date_now = datetime.now()
-        date_now = time.mktime(date_now.timetuple())
-        row_unix_time = time.mktime(head[2].timetuple())
-        remaining_time = row_unix_time - date_now
-        q_elt = (head[0], head[1], head[2], remaining_time)
-        if remaining_time == 0:
-            configure = ConfigParser()
-            path_script = configure.get(head[1], 'path')
-            os.system(f"python {path_script} {head[0]} {head[1]} ")  # TODO create Thread
-                                                                     # TODO insert into sequnecer table
-                                                                     # TODO follow thread
-                                                                     # TODO put head
-                                                                     # TODO insert to runing task table or if head[1] = SSPS -> insert in ssps table
-                                                                     # TODO insert to runing task table or if head[1] = SSPS -> insert in media manager (traitement HTMR)table
-            print("OK........OK")
-
-            q.get(head)
-    #
-
-        print(q_elt)
-
-
-    #
-
-
-    time.sleep(1)
+def update_column(task_number,status:str=''):
+    schema = 'groundstation'
+    table_name = 'planifier'
+    column='status'
+    sql = (f'''update {schema}."{table_name}"  set {column} ='{status}' 
+            where {table_name}.task_number = {task_number}''')
+    curr.execute(sql)
+    coonn.commit()
+    print(colored(f"""case is finished""", 'green', attrs=['reverse', 'blink']))
 
 
 
 
 
+def create_task():
+    task_list=[]
+    q = queue.Queue()
+    sql = (f'''SELECT task_number , task_name , task_time_start  FROM groundstation.planifier
+           where planifier."task_time_start" > now() at time zone 'utc'
+           order by planifier."task_time_start"
+           limit 1
+           
+           ''')
+    curr.execute(sql)
+    rows = curr.fetchall()
+    for row in rows:
+        task_list.append(row)
+        q.put(row)
 
+
+
+
+    while not q.empty():
+        print ("#####################################################################")
+        for head in list(q.queue):
+            date_now = datetime.now()
+            date_now = time.mktime(date_now.timetuple())
+            row_unix_time = time.mktime(head[2].timetuple())
+            remaining_time = row_unix_time - date_now
+            q_elt = (head[0], head[1], head[2], remaining_time)
+
+            if remaining_time == 0:
+                try:
+                    configure = ConfigParser()
+                    configure.read('tasks.ini')
+                    path_script = configure.get(head[1], 'path')
+                    print(path_script , head[0] , head [1])
+                    os.system(f"python {path_script} {head[0]} {head[1]} ") # TODO create Thread
+                    update_column(head[0],'in progress')
+                                                                             # TODO insert into sequnecer table
+                                                                             # TODO follow thread
+                                                                             # TODO put head
+                                                                             # TODO insert to runing task table or if head[1] = SSPS -> insert in ssps table
+                                                                             # TODO insert to runing task table or if head[1] = SSPS -> insert in media manager (traitement HTMR)table
+
+                    q.get(head)
+
+                except Exception as e:
+                    print(colored(f"{e}","red") )
+                    q.get(head)
+                    q.get(head)
+
+            print(q_elt)
+        time.sleep(1)
+
+    create_task()
+
+
+
+def create():
+
+    for i in range(1000):
+        print(i)
+
+
+t1 = threading.Thread(target=create_task(), args=())
+#t2 = threading.Thread(target=create(), args=())
+t1.start()  # starting thread 1
+#t2.start()  # starting thread 1
+
+print(threading.get_ident())
+#t2 = multiprocessing.Process(target=create2(), args=())
+#t1 = multiprocessing.Process(target=create_task(), args=())
+
+
+
+
+
+#print("**************************************************************")
+#create()
 
 
 
--- test_queue.py
+++ test_queue.py
@@ -1,64 +1,8 @@
+import threading
+import time
 
-p=0
-while p<100:
-    sql = (f'''SELECT task_number , task_name , task_time_start  FROM groundstation.planifier
-           where planifier."task_time_start" > now() at time zone 'utc'
-           order by planifier."task_time_start"
-           asc limit 5
-           ''')
-    curr.execute(sql)
-    rows = curr.fetchall()
-    date_now = datetime.now()
-    date_now = time.mktime(date_now.timetuple())
-    for row in rows :
-        row_unix_time = time.mktime(row[2].timetuple())
-        remaining_time = row_unix_time - date_now
-        tab = [row[0], row[1], row[2], remaining_time]
-        print(tab)
-
-        if remaining_time == 0:
-            print("OK........OK")
-            break
-
-
-    print(p)
-    p+=1
-    sql = (f'''SELECT task_number , task_name , task_time_start  FROM groundstation.planifier
-           where planifier."task_time_start" > now() at time zone 'utc'
-           order by planifier."task_time_start"
-           asc limit 5
-           ''')
-    curr.execute(sql)
-    rows = curr.fetchall()
-    time.sleep(1)
-
-
-
-###############################################################################################
-
-
-
-
-p=0
-while p<100:
-    sql = (f'''SELECT task_number , task_name , task_time_start  FROM groundstation.planifier
-           where planifier."task_time_start" > now() at time zone 'utc'
-           order by planifier."task_time_start"
-           asc limit 5
-           ''')
-    curr.execute(sql)
-    row = curr.fetchone()
-
-    date_now = datetime.now()
-    date_now = time.mktime(date_now.timetuple())
-    row_unix_time = time.mktime(row[2].timetuple())
-    remaining_time = row_unix_time - date_now
-    tab = [row[0], row[1], row[2], remaining_time]
-    print(tab)
-
-
-
-    print(p)
-    p+=1
-
-    time.sleep(1)
+for _ in range(20):
+    time.sleep(3)
+    print("\n", threading.currentThread().getName())
+    print("current thread", threading.current_thread())
+    print("active threads ", threading.active_count())
